c5bca856e72945986e1437a9bf6e028660f656a5
ramesh singh <rameshsingh2307@gmail.com>
2022-02-08 14:47:44 +0530

    Demo of test package is modified

--- Demo/src/com/ram/test/Demo.java
+++ Demo/src/com/ram/test/Demo.java
@@ -3,8 +3,8 @@ package com.ram.test;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-
+		System.out.println("Hello world testing..");
+		
 	}
 
 }
